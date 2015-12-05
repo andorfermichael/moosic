45769f604234877fd0cc68846557bb4b6baca4b6
Berneau <bsteger.mmt-b2013@fh-salzburg.ac.at>
2015-12-05 12:44:02 +0100

    [chore] added seeds

--- moosic/db/seeds.rb
+++ moosic/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create([
+  {username: "MusterMax", email: "muster.max@muster.com", password: "secret", picture_url: "profile.com"},
+  {username: "ProfiPeter", email: "profi.peter@profi.com", password: "anonymous", picture_url: "profile.com"},
+  {username: "NilpferdNils", email: "nilpferd.nils@nilpferd.com", password: "amazonas"}
+])
+
+
+Song.create([
+  {titel: "Radioactive", source: "ImagineDragonsVEVO", thumbnail_url: "https://i.ytimg.com/vi/ktvTqknDobU/mqdefault.jpg", song_url: "ktvTqknDobU", host: "youtube", year: DateTime.new(2015, 9, 1,)},
+  {titel: "WILD - Back To You", source: "MrSuicideSheep", thumbnail_url: "https://i.ytimg.com/vi/HtcWvsiQkZE/mqdefault.jpg", song_url: "HtcWvsiQkZE", host: "youtube", year: DateTime.new(2015, 12, 4)}
+])
+
+
+Playlist.create([
+  {name: "Chillout", user_id: 1},
+  {name: "LoungeMusic", user_id: 1},
+  {name: "Motivational", user_id: 3}
+])
+
+
+Track.create([
+  {position: 1, playlist_id: 1, song_id: 1},
+  {position: 2, playlist_id: 1, song_id: 2},
+  {position: 1, playlist_id: 2, song_id: 1}
+])
