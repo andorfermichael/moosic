23ea8a921e6fffe281418842c9642bbeba07e89b
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2015-12-25 12:27:45 +0100

    [feature] implement playlist list in user profile

--- moosic/app/controllers/users_controller.rb
+++ moosic/app/controllers/users_controller.rb
@@ -6,7 +6,23 @@ class UsersController < ApplicationController
   end
 
   def show
+    # Get all playlists from current user
+    @playlists = Playlist.where(user_id: session[:user_id]).all
 
+    # For each playlist store the image url of the first song of the playlist plus the count of songs
+    # as an additional information
+    @playlists.each do |playlist|
+      track = Track.where('playlist_id = ? AND position = ?', playlist.id, 1).first
+      if track != nil
+        song = Song.find(track.song_id)
+        playlist.image_url = song.thumbnail_url
+        playlist.count_songs = Playlist.find(playlist.id).songs.size
+        true
+      else
+        playlist.image_url = 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=350&h=150'
+        playlist.count_songs = 0
+      end
+    end
   end
 
   # Create new user object when rendering new user view
