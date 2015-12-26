97f80f8d44ab703b0a1b2e5f0582b19f31943357
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2015-12-26 17:01:09 +0100

    [feature] add responsive design for user profile

--- moosic/app/assets/javascripts/users.js
+++ moosic/app/assets/javascripts/users.js
@@ -3,14 +3,23 @@
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
 
-$(document).ready(function(){
-  // Show menu
-  $('.user-info-top').on('mouseenter', function(){
-      $('.user-menu').show();
-  });
+$(document).ready(function() {
+
+  if (window.width >= 768) {
+      // Show menu
+      $('.user-info-top').on('mouseenter', function() {
+          $('.user-menu').show();
+      });
+  }
+  else {
+      // Show menu
+      $('.user-profile-header').on('mouseenter', function() {
+          $('.user-menu').show();
+      });
+  }
 
   // Hide menu
-  $('body').on('click', function(){
+  $('body').on('click', function() {
       $('.user-menu').hide();
   });
 
