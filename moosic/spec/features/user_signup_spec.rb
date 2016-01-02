94a6571c94a66493ccf8190b2ffb78ee952fa164
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2016-01-02 19:39:57 +0100

    [test] add test with scenario - email has no valid format

--- moosic/spec/features/user_signup_spec.rb
+++ moosic/spec/features/user_signup_spec.rb
@@ -49,6 +49,18 @@ feature "native registration process", :type => :feature do
     expect(page).to have_content "Email can't be blank"
   end
 
+  scenario "email has no valid format" do
+    visit signup_path
+    within(".input-container") do
+      fill_in 'user_name', :with => 'John Doe'
+      fill_in 'user_email', :with => '@john.doe.at'
+      fill_in 'user_password', :with => 'TestMeIfYouC4n'
+      fill_in 'user_password_confirmation', :with => 'TestMeIfYou4Can'
+    end
+    click_button 'Sign up'
+    expect(page).to have_content "Email-Address has no valid format"
+  end
+
   scenario "password and password confirmation do NOT match" do
     visit signup_path
     within(".input-container") do
