c3afefc2b855c563ac13bf5a4985c33eaacb518d
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2016-01-02 19:25:58 +0100

    [test] add test with scenario - username is too short

--- moosic/spec/features/user_signup_spec.rb
+++ moosic/spec/features/user_signup_spec.rb
@@ -12,4 +12,16 @@ feature "native registration process", :type => :feature do
     click_button 'Sign up'
     expect(page).to have_content "Password confirmation doesn't match Password"
   end
+
+  scenario "name is too short" do
+    visit signup_path
+    within(".input-container") do
+      fill_in 'user_name', :with => 'Joe'
+      fill_in 'user_email', :with => 'john.doe@example.com'
+      fill_in 'user_password', :with => 'TestMeIfYouC4n'
+      fill_in 'user_password_confirmation', :with => 'TestMeIfYou4Can'
+    end
+    click_button 'Sign up'
+    expect(page).to have_content "Name is too short (minimum is 4 characters)"
+  end
 end
