9751d25105efae08eaf1853a539061d7f79ae062
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2016-01-02 21:31:09 +0100

    [test] add test with scenario - enter invalid email

--- moosic/spec/features/user_login_spec.rb
+++ moosic/spec/features/user_login_spec.rb
@@ -19,4 +19,23 @@ feature "native login process", :type => :feature do
     click_button 'Log in'
     expect(page).to have_content "Email and Password invalid! Try again!"
   end
+
+  scenario "invalid username" do
+    visit signup_path
+    within(".input-container") do
+      fill_in 'user_name', :with => 'John Doe'
+      fill_in 'user_email', :with => 'john.doe@example.com'
+      fill_in 'user_password', :with => 'I4mavstrongPassw0rd'
+      fill_in 'user_password_confirmation', :with => 'I4mavstrongPassw0rd'
+    end
+    click_button 'Sign up'
+
+    visit root_path
+    within(".input-container") do
+      fill_in 'session_email', :with => 'john.doe@wrong.com'
+      fill_in 'session_password', :with => 'I4mavstrongPassw0rd'
+    end
+    click_button 'Log in'
+    expect(page).to have_content "Email and Password invalid! Try again!"
+  end
 end
