e9be3f134c32c883ad3a45e8105aa9fb949ce13f
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2016-01-03 12:18:34 +0100

    [test] add test scenario - show playlists created by logged in user

---
+++ moosic/spec/features/user_profile_spec.rb
@@ -0,0 +1,33 @@
+require "spec_helper"
+
+feature "user profile content and features", :type => :feature do
+  scenario "user profile lists playlists created by the logged in user" do
+    user = create(:user)
+    playlist1 = create(:playlist)
+    playlist2 = FactoryGirl.create(:playlist, name: 'Chillout', user_id: 1)
+    playlist3 = FactoryGirl.create(:playlist, name: 'Progressive House', user_id: 2)
+    song1 = create(:song)
+    song2 = create(:song, titel: "Payback", source: "Dimitri Vangelis & Wyman", thumbnail_url: "", song_url: "147229671", host: "soundcloud", year: DateTime.new(2015, 12, 4))
+    song2 = create(:song, titel: "WILD - Back To You", source: "MrSuicideSheep", thumbnail_url: "https://i.ytimg.com/vi/HtcWvsiQkZE/mqdefault.jpg", song_url: "HtcWvsiQkZE", host: "youtube", year: DateTime.new(2015, 12, 4))
+    track1 = create(:track)
+    track2 = create(:track, position: 2, playlist_id: 1, song_id: 2)
+    track3 = create(:track, position: 1, playlist_id: 2, song_id: 3)
+
+    visit root_path
+    within(".input-container") do
+      fill_in 'session_email', :with => 'john.doe@example.com'
+      fill_in 'session_password', :with => 'Myvery2trongPa22w0rd'
+    end
+    click_button 'Log in'
+
+    expect(first("a[href='/playlists/1']")).to have_content "EDM"
+    expect(first("a[href='/playlists/1']")).to have_content "created on 03.01.2016"
+    expect(first("a[href='/playlists/1']")).to have_content "2 songs"
+
+    expect(first("a[href='/playlists/2']")).to have_content "Chillout"
+    expect(first("a[href='/playlists/2']")).to have_content "created on 03.01.2016"
+    expect(first("a[href='/playlists/2']")).to have_content "1 songs"
+
+    expect(page).not_to have_content "Progressive House"
+  end
+end
