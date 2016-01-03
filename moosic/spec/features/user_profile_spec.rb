d4c83ba40fec52df584185b22c4f7081bd484679
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2016-01-03 13:04:24 +0100

    [test] add test scenario - user profile contains placeholder image in sidebar

--- moosic/spec/features/user_profile_spec.rb
+++ moosic/spec/features/user_profile_spec.rb
@@ -96,4 +96,17 @@ feature "user profile content and features", :type => :feature do
 
     expect(page.find('.img-circle-small')['src']).to eq("http://gravatar.com/avatar/8eb1b522f60d11fa897de1dc6351b7e8?default=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fb%2Fb1%2FPortrait_placeholder.png&secure=false&size=512")
   end
+
+  scenario "user profile contains placeholder image in sidebar" do
+    user = create(:user)
+
+    visit root_path
+    within(".input-container") do
+      fill_in 'session_email', :with => 'john.doe@example.com'
+      fill_in 'session_password', :with => 'Myvery2trongPa22w0rd'
+    end
+    click_button 'Log in'
+
+    expect(page.find('.img-circle-big')['src']).to eq("http://gravatar.com/avatar/8eb1b522f60d11fa897de1dc6351b7e8?default=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fb%2Fb1%2FPortrait_placeholder.png&secure=false&size=512")
+  end
 end
