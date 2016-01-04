7616c4daac0c6f58e0828b75ff4d671d5eb6e0b3
Berneau <bsteger.mmt-b2013@fh-salzburg.ac.at>
2016-01-04 22:06:24 +0100

    [chore] add popup window for playlists

--- moosic/app/assets/javascripts/search.js
+++ moosic/app/assets/javascripts/search.js
@@ -1,153 +1,150 @@
 window.onload = function() {
 
-  initializeSoundcloud()
+	initializeSoundcloud()
 
-  $('#Yt').click(function() {
-    $('#Yt').addClass('active');
-    $('#Sc').removeClass('active');
-    $('.Sc').addClass('hidden');
-    $('.Yt').removeClass('hidden');
-  });
+	$('#Yt').click(function() {
+		$('#Yt').addClass('active');
+		$('#Sc').removeClass('active');
+		$('.Sc').addClass('hidden');
+		$('.Yt').removeClass('hidden');
+	});
 
-  $('#Sc').click(function() {
-    $('#Sc').addClass('active');
-    $('#Yt').removeClass('active');
-    $('.Yt').addClass('hidden');
-    $('.Sc').removeClass('hidden');
-  });
+	$('#Sc').click(function() {
+		$('#Sc').addClass('active');
+		$('#Yt').removeClass('active');
+		$('.Yt').addClass('hidden');
+		$('.Sc').removeClass('hidden');
+	});
 
-  $(document).on("click", ".add-button", function(e) {
-    console.log(e);
+	$('.cancel').click(function() {
+		$('.playlist-popup').addClass('hidden');
+	});
 
-    renderPlaylistDiv(e);
-  });
+	//$('')
+
+	$(document).on("click", ".add-button", function(e) {
+		console.log(e);
+
+		$('.playlist-popup').removeClass('hidden');
+	});
 }
 
 
 var resultAmount = 10;
 
 function searchByKeyword() {
-  document.getElementById('result-container').innerHTML = '';
-  var keyword = $('#searchInput').val();
+	document.getElementById('result-container').innerHTML = '';
+	var keyword = $('#searchInput').val();
 
-  youtubeRequest(keyword);
-  soundcloudRequest(keyword);
+	youtubeRequest(keyword);
+	soundcloudRequest(keyword);
 }
 
 
 function youtubeRequest(keyword) {
-  var request = gapi.client.youtube.search.list({
-    maxResults: resultAmount,
-    part: 'snippet',
-    type: 'video',
-    q: keyword
-  });
-  request.execute(generateYoutubeList);
+	var request = gapi.client.youtube.search.list({
+		maxResults: resultAmount,
+		part: 'snippet',
+		type: 'video',
+		q: keyword
+	});
+	request.execute(generateYoutubeList);
 }
 
 
 function soundcloudRequest(keyword) {
-  SC.get('/tracks', {
-    q: keyword,
-    limit: resultAmount
-  }).then(function(response) {
-    generateSoundcloudList(response);
-  });
+	SC.get('/tracks', {
+		q: keyword,
+		limit: resultAmount
+	}).then(function(response) {
+		generateSoundcloudList(response);
+	});
 }
 
 
 function generateYoutubeList(response) {
-  var parent = document.getElementById('result-container');
+	var parent = document.getElementById('result-container');
 
-  for (var i = 0; i < response.items.length; i++) {
-    var item = response.items[i];
+	for (var i = 0; i < response.items.length; i++) {
+		var item = response.items[i];
 
-    var listItem = generateListItem(item.id.videoId,
-      item.snippet.thumbnails.medium.url,
-      item.snippet.title,
-      item.snippet.channelTitle,
-      'Yt');
+		var listItem = generateListItem(item.id.videoId,
+			item.snippet.thumbnails.medium.url,
+			item.snippet.title,
+			item.snippet.channelTitle,
+			'Yt');
 
-    parent.appendChild(listItem);
-  };
+		parent.appendChild(listItem);
+	};
 }
 
 
 function generateSoundcloudList(response) {
-  var parent = document.getElementById('result-container');
-  //console.log(response);
+	var parent = document.getElementById('result-container');
+	//console.log(response);
 
-  for (var i = 0; i < response.length; i++) {
-    var item = response[i];
+	for (var i = 0; i < response.length; i++) {
+		var item = response[i];
 
-    var listItem = generateListItem(item.id,
-      item.artwork_url,
-      item.title,
-      item.user.username,
-      'Sc');
+		var listItem = generateListItem(item.id,
+			item.artwork_url,
+			item.title,
+			item.user.username,
+			'Sc');
 
-    parent.appendChild(listItem);
-  };
-  $('.Sc').addClass('hidden');
+		parent.appendChild(listItem);
+	};
+	$('.Sc').addClass('hidden');
 }
 
 
 function generateListItem(id, thumbnail, title, channel, host) {
-  // create list item, videoId tag and host class
-  var listItem = document.createElement('li');
-  var hostClass = document.createAttribute('class');
-  hostClass.value = host;
-  listItem.setAttributeNode(hostClass);
+	// create list item, videoId tag and host class
+	var listItem = document.createElement('li');
+	var hostClass = document.createAttribute('class');
+	hostClass.value = host;
+	listItem.setAttributeNode(hostClass);
 
-  // create thumbnail
-  var tn = document.createElement('img');
-  var tnClass = document.createAttribute('class');
-  tnClass.value = "tn";
-  var tnSrc = document.createAttribute('src');
-  if(thumbnail != null) tnSrc.value = thumbnail;
-  else if (host == "Yt") tnSrc.value = "/assets/yt-placeholder.png";
-  else if (host == "Sc") tnSrc.value = "/assets/sc-placeholder.png";
-  tn.setAttributeNode(tnSrc);
-  tn.setAttributeNode(tnClass);
+	// create thumbnail
+	var tn = document.createElement('img');
+	var tnClass = document.createAttribute('class');
+	tnClass.value = "tn";
+	var tnSrc = document.createAttribute('src');
+	if(thumbnail != null) tnSrc.value = thumbnail;
+	else if (host == "Yt") tnSrc.value = "/assets/yt-placeholder.png";
+	else if (host == "Sc") tnSrc.value = "/assets/sc-placeholder.png";
+	tn.setAttributeNode(tnSrc);
+	tn.setAttributeNode(tnClass);
 
-  // create menu-button
-  var add = document.createElement('img');
-  var addClass = document.createAttribute('class');
-  addClass.value = "add-button";
-  var addSrc = document.createAttribute('src');
-  addSrc.value = "/assets/add.png";
-  var idTag = document.createAttribute('data-video-id');
-  idTag.value = id;
-  add.setAttributeNode(addSrc);
-  add.setAttributeNode(idTag);
-  add.setAttributeNode(addClass);
+	// create menu-button
+	var add = document.createElement('img');
+	var addClass = document.createAttribute('class');
+	addClass.value = "add-button";
+	var addSrc = document.createAttribute('src');
+	addSrc.value = "/assets/add.png";
+	var idTag = document.createAttribute('data-video-id');
+	idTag.value = id;
+	add.setAttributeNode(addSrc);
+	add.setAttributeNode(idTag);
+	add.setAttributeNode(addClass);
 
-  // create video title
-  var titleSpan = document.createElement('span');
-  titleSpan.appendChild(document.createTextNode(title));
+	// create video title
+	var titleSpan = document.createElement('span');
+	titleSpan.appendChild(document.createTextNode(title));
 
-  // create channel title
-  var channelSpan = document.createElement('span');
-  channelSpan.appendChild(document.createTextNode(channel));
+	// create channel title
+	var channelSpan = document.createElement('span');
+	channelSpan.appendChild(document.createTextNode(channel));
 
-  // append to listItem
-  listItem.appendChild(tn);
-  listItem.appendChild(add);
-  listItem.appendChild(titleSpan);
-  listItem.appendChild(channelSpan);
+	// append to listItem
+	listItem.appendChild(tn);
+	listItem.appendChild(add);
+	listItem.appendChild(titleSpan);
+	listItem.appendChild(channelSpan);
 
-  return listItem;
+	return listItem;
 }
 
-
-
-function renderPlaylistDiv(e) {
-  var div = document.createElement('div');
-  var divClass = document.createAttribute('class');
-  divClass.value = "playlist-popup";
-
-  
-}
 
 
 
@@ -164,17 +161,17 @@ function renderPlaylistDiv(e) {
 
 // Youtube Api Setup functions
 function onClientLoad() {
-  gapi.client.load('youtube', 'v3', onYoutubeApiLoad);
+	gapi.client.load('youtube', 'v3', onYoutubeApiLoad);
 }
 
 function onYoutubeApiLoad() {
-  gapi.client.setApiKey('AIzaSyCUnKMWe0bzmqB5jMasNVLfY4cYupf_auY')
+	gapi.client.setApiKey('AIzaSyCUnKMWe0bzmqB5jMasNVLfY4cYupf_auY')
 }
 
 
 // Soundcloud Api Setup function
 function initializeSoundcloud() {
-  SC.initialize({
-    client_id: '030f3558914f2b95e6535c76ecf34e53'
-  });
+	SC.initialize({
+		client_id: '030f3558914f2b95e6535c76ecf34e53'
+	});
 }
