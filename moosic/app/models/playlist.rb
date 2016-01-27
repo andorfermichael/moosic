a896bbbdec0fb1e0db6c47806717fdb7b53bfebe
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2016-01-27 13:19:29 +0100

    [refactor] improve models by replacement of hash syntax with json syntax

--- moosic/app/models/playlist.rb
+++ moosic/app/models/playlist.rb
@@ -1,12 +1,12 @@
 class Playlist < ActiveRecord::Base
   belongs_to :user
   has_many :tracks, dependent: :destroy
-  has_many :songs, :through => :tracks
+  has_many :songs, through: :tracks
 
   attr_accessor :image_url
   attr_accessor :count_songs
 
   def self.search(query)
-    where("name LIKE ?", "%#{query}%" )
+    where('name LIKE ?', "%#{query}%")
   end
 end
--- moosic/app/models/song.rb
+++ moosic/app/models/song.rb
@@ -1,4 +1,4 @@
 class Song < ActiveRecord::Base
   has_many :tracks, dependent: :destroy
-  has_many :playlists, :through => :tracks
+  has_many :playlists, through: :tracks
 end
