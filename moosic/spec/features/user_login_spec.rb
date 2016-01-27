cf3610585df105367c356ee6bd0ee705cd7a7817
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2016-01-27 13:41:18 +0100

    [refactor] improve tests and according files via replacement of double quotes with single quotes, replacement of hash syntax with json syntax and deletion of unnecessary instantiations

--- moosic/spec/features/user_login_spec.rb
+++ moosic/spec/features/user_login_spec.rb
@@ -1,60 +1,60 @@
-require "spec_helper"
+require 'spec_helper'
 
-feature "native login process", :type => :feature do
-  scenario "invalid password" do
+feature 'native login process', type: :feature do
+  scenario 'invalid password' do
     visit signup_path
-    within(".input-container") do
-      fill_in 'user_name', :with => 'John Doe'
-      fill_in 'user_email', :with => 'john.doe@example.com'
-      fill_in 'user_password', :with => 'I4mavstrongPassw0rd'
-      fill_in 'user_password_confirmation', :with => 'I4mavstrongPassw0rd'
+    within('.input-container') do
+      fill_in 'user_name', with: 'John Doe'
+      fill_in 'user_email', with: 'john.doe@example.com'
+      fill_in 'user_password', with: 'I4mavstrongPassw0rd'
+      fill_in 'user_password_confirmation', with: 'I4mavstrongPassw0rd'
     end
     click_button 'Sign up'
 
     visit root_path
-    within(".input-container") do
-      fill_in 'session_email', :with => 'john.doe@example.com'
-      fill_in 'session_password', :with => 'WrongPassword'
+    within('.input-container') do
+      fill_in 'session_email', with: 'john.doe@example.com'
+      fill_in 'session_password', with: 'WrongPassword'
     end
     click_button 'Log in'
-    expect(page).to have_content "Email and Password invalid! Try again!"
+    expect(page).to have_content 'Email and Password invalid! Try again!'
   end
 
-  scenario "invalid username" do
+  scenario 'invalid username' do
     visit signup_path
-    within(".input-container") do
-      fill_in 'user_name', :with => 'John Doe'
-      fill_in 'user_email', :with => 'john.doe@example.com'
-      fill_in 'user_password', :with => 'I4mavstrongPassw0rd'
-      fill_in 'user_password_confirmation', :with => 'I4mavstrongPassw0rd!'
+    within('.input-container') do
+      fill_in 'user_name', with: 'John Doe'
+      fill_in 'user_email', with: 'john.doe@example.com'
+      fill_in 'user_password', with: 'I4mavstrongPassw0rd'
+      fill_in 'user_password_confirmation', with: 'I4mavstrongPassw0rd!'
     end
     click_button 'Sign up'
 
     visit root_path
-    within(".input-container") do
-      fill_in 'session_email', :with => 'john.doe@wrong.com'
-      fill_in 'session_password', :with => 'I4mavstrongPassw0rd!'
+    within('.input-container') do
+      fill_in 'session_email', with: 'john.doe@wrong.com'
+      fill_in 'session_password', with: 'I4mavstrongPassw0rd!'
     end
     click_button 'Log in'
-    expect(page).to have_content "Email and Password invalid! Try again!"
+    expect(page).to have_content 'Email and Password invalid! Try again!'
   end
 
-  scenario "valid login" do
+  scenario 'valid login' do
     visit signup_path
-    within(".input-container") do
-      fill_in 'user_name', :with => 'John Doe'
-      fill_in 'user_email', :with => 'john.doe@example.com'
-      fill_in 'user_password', :with => 'I4mavstrongPassw0rd!'
-      fill_in 'user_password_confirmation', :with => 'I4mavstrongPassw0rd!'
+    within('.input-container') do
+      fill_in 'user_name', with: 'John Doe'
+      fill_in 'user_email', with: 'john.doe@example.com'
+      fill_in 'user_password', with: 'I4mavstrongPassw0rd!'
+      fill_in 'user_password_confirmation', with: 'I4mavstrongPassw0rd!'
     end
     click_button 'Sign up'
 
     visit root_path
-    within(".input-container") do
-      fill_in 'session_email', :with => 'john.doe@example.com'
-      fill_in 'session_password', :with => 'I4mavstrongPassw0rd!'
+    within('.input-container') do
+      fill_in 'session_email', with: 'john.doe@example.com'
+      fill_in 'session_password', with: 'I4mavstrongPassw0rd!'
     end
     click_button 'Log in'
-    expect(page).to have_content "Login successfully!"
+    expect(page).to have_content 'Login successfully!'
   end
 end
